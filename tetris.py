5017769020104358ba9d48b6b23f9c4c9d1a995b
Elin Ludi <elin.ludi@students.lerbermatt.ch>
2023-05-26 16:25:23 +0200

    Blöcke erstellt, so wie auch die Farben. Sie gehen nach unten doch man kann sie noch nicht mit der Taste bewegen.

--- tetris.py
+++ tetris.py
@@ -1,20 +1,47 @@
 import pygame
+import random
 
 colors = [
     (0,0,0),
-    (120,37,179),
-    (100,179,179),
-    (80,34,22),
-    (80,134,22),
-    (180,34,22),
-    (180,34,122)
+    (0,240,240), #4 in einer Reihe
+    (0,0,240), #Reverse L
+    (240,160,0), #L
+    (240,240,0), #Block
+    (0,240,0), #S
+    (160,0,240), #T
+    (240,0,0) #Reverse S
 ]
+
+class Figure:
+    x = 0
+    y = 0
+
+    Figures = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]], #4 in einer Reihe
+        [[0, 4, 5, 6], [1, 2, 5, 9], [1, 5, 9, 8], [4, 5, 6, 10]], #Reverse L
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11],[3, 5, 6, 7]], #L
+        [[1, 2, 5, 6]], #Block
+        [[6, 7, 9, 10], [1, 5, 6, 10]], #S
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]], #T
+        [[4, 5, 9, 10], [2, 6, 5, 9]] #Rrverse S
+    ]
+    def __init__(self, x_coord, y_coord):
+        self.x = x_coord
+        self.y = y_coord
+        self.type = random.randint(0,len(self.Figures)-1)
+        self.color = colors[self.type+1]
+        self.rotation = 0
+    def image(self):
+        return self.Figures[self.type][self.rotation]
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.Figures[self.type])
 class Tetris:
     height = 0
     width = 0
     field = []
     score = 0
     state = "start"
+    Figure = None
 
     def __init__(self, _height, _width):
         self.height = _height
@@ -27,8 +54,28 @@ class Tetris:
             for j in range(_width):
                 new_line.append(0)
             self.field.append(new_line)
+        self.new_figure()
 
+    def new_figure(self):
+        self.Figure = Figure(3, 0)
+    def go_down(self):
+        self.Figure.y += 1
 
+    def side(self, dx):
+        old_x = self.Figure.x
+        self.Figure.x += dx
+
+    def left(self):
+        self.side(-1)
+
+    def right(self):
+        self.side(1)
+
+    def down(self):
+        pass
+
+    def rotate(self):
+        self.Figure.rotate()
 
 
 pygame.init()
@@ -36,7 +83,7 @@ screen = pygame.display.set_mode([400, 500])
 pygame.display.set_caption("Tetris")
 
 done = False
-fps = 25
+fps = 2
 clock = pygame.time.Clock()
 counter = 0
 zoom = 23
@@ -51,14 +98,13 @@ WHITE = (255, 255, 255)
 GRAY = (100, 100, 100)
 EGGSHELL = (238, 238, 228)
 
-
-
 while not done:
+    if game.state == "start":
+        game.go_down()
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_F:
-                game.rotate()
-            if event.key == pygame.K_S:
+            if event.key == pygame.K_UP:
                 game.rotate()
             if event.key == pygame.K_DOWN:
                 pressing_down = True
@@ -67,12 +113,7 @@ while not done:
             if event.key == pygame.K_RIGHT:
                 pressing_right = True
 
-        if pressing_down:
-            game.down()
-        if pressing_left:
-            game.left()
-        if pressing_right:
-            game.right()
+
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
@@ -81,6 +122,13 @@ while not done:
                 pressing_left = False
             if event.key == pygame.K_RIGHT:
                 pressing_right = False
+
+        if pressing_down:
+            game.down()
+        if pressing_left:
+            game.left()
+        if pressing_right:
+            game.right()
 
     screen.fill(color=EGGSHELL)
     for i in range(game.height):
@@ -92,6 +140,13 @@ while not done:
                 color = colors[game.field[i][j]]
                 just_border = 0
             pygame.draw.rect(screen, color, [80+j*zoom, 10+i*zoom, zoom, zoom], just_border)
+    if game.Figure is not None:
+        for i in range(4):
+            for j in range(4):
+                p = i * 4 + j
+                if p in game.Figure.image():
+                    pygame.draw.rect(screen, game.Figure.color,
+                                     [80+(j + game.Figure.x)*zoom, 10+(i+ game.Figure.y)*zoom, zoom, zoom])
 
     pygame.display.flip()
     clock.tick(fps)
