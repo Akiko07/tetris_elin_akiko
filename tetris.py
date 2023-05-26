10f2da3b3c40284210d4a5d37d22c999cb75fc67
rossi <akiko.rossi@students.lerbermatt.ch>
2023-05-26 13:44:28 +0200

    bildschirm fertig erstellt, tasten definiert, farbe definiert

--- tetris.py
+++ tetris.py
@@ -1,5 +1,25 @@
 import pygame
 
+class Tetris:
+    height = 0
+    width = 0
+    field = []
+    score= 0
+    state = "start"
+
+    def __init__(self, _height, _width):
+        self.height = _height
+        self.width = _width
+        self.field = []
+        self.score = 0
+        self.state = "start"
+        for i in range(_height):
+            new_line = []
+            for j in range(_width):
+                new_line.append(0)
+            self.field.append(new_line)
+
+
 
 
 pygame.init()
@@ -11,20 +31,24 @@ fps = 25
 clock = pygame.time.Clock()
 counter = 0
 
-game = Tetris()
+game = Tetris(20, 10)
 pressing_down = False
 pressing_left = False
 pressing_right = False
 
-BLACK = (0,0,0)
-WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (100, 100, 100)
+EGGSHELL = (238, 238, 228)
 
 
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_F:
+                game.rotate()
+            if event.key == pygame.K_S:
                 game.rotate()
             if event.key == pygame.K_DOWN:
                 pressing_down = True
@@ -48,7 +72,12 @@ while not done:
             if event.key == pygame.K_RIGHT:
                 pressing_right = False
 
-    screen.fill(color=)
+    screen.fill(color=EGGSHELL)
+    for i in range(game.height):
+        for j in range(game.width):
+            pygame.draw.rect(screen, GRAY, [])
+
+    clock.tick(fps)
 
 
 
