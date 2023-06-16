f5e7ae20e186527041ae517ee3e1b3bc42a07c55
Elin Ludi <elin.ludi@students.lerbermatt.ch>
2023-06-16 13:09:21 +0200

    farben geändert

--- tetris.py
+++ tetris.py
@@ -3,13 +3,13 @@ import random
 
 colors = [
     (0,0,0),
-    (0,240,240), #4 in einer Reihe
-    (0,0,240), #Reverse L
-    (240,160,0), #L
-    (240,240,0), #Block
-    (0,240,0), #S
-    (160,0,240), #T
-    (240,0,0) #Reverse S
+    (204,236,239), #4 in einer Reihe
+    (117,137,191), #Reverse L
+    (253,202,162), #L
+    (255,250,129), #Block
+    (249,140,182), #S
+    (193,179,215), #T
+    (281,225,174) #Reverse S
 ]
 
 class Figure:
